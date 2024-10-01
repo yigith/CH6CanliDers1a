876e15b4445f59f5eb816a364bf24a9256a1a4f3
yigith <yigith1@gmail.com>
2024-10-02 01:17:38 +0700

    Add project files.

---
+++ ArgsFarkindalik/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ArgsFarkindalik
+{
+    internal class Program
+    {
+        // console üzerinden girilen parametreleri
+        // args ile alabiliriz ve kullanabiliriz
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello " + args[0]);
+        }
+    }
+}
---
+++ CH6CanliDers1a/Program.cs
@@ -0,0 +1,57 @@
+﻿// bir de burada CTRL+. ile programı Main metodulu hale getirin
+
+// Kasıtlı hata yapıyorum
+int sifir = 0;
+// Console.WriteLine(1 / sifir);
+
+// cw [TAB]
+Console.WriteLine("Merhaba Dünya!");
+
+// programı çalıştırma F5 ya da CTRL+F5
+
+// alt + yukarı aşağı ok ile bir satırı taşıyabilirsiniz
+
+// bir büyük projede çalışıyorsun ve bir yer eksik kaldı
+// yarın tamamlamak istiyorsun ve unutmak istemiyorsun
+
+int toplam = 2 + 2;
+
+// todo: toplam sonucunu ekrana yazdır
+//Console.WriteLine(toplam);
+
+// satır çoğaltma: imleç satır üzerindeyken
+// önce CTRL+C sonra istediğiniz kadar CTRL+V
+// (hiçbir şey seçmeye gerek yok)
+Console.WriteLine("Oley!");
+Console.WriteLine("Oley!");
+Console.WriteLine("Oley!");
+
+// kaynağına gitmek için tür üzerinde sağ tuş>go to definition
+// ya da tür üzerinde imleç varken f12
+string cumle = "Ali ata bak";
+
+// bir satırı ya da seçimi yoruma
+// CTRL K, C ile yoruma alabiliriz : comment
+// CTRL K, U ile yoruma alabiliriz : uncomment
+Console.WriteLine("bu satırı yoruma alıp kaldırın");
+
+// hocam ben çok düzensiz kod yazıyorum ama 
+// kodumu düzene sokmak için bir kısayol var mı:
+// CTRL K, D
+
+// if üzerinde CTRL+. kısayoluyla quick actions menüsünü aç
+// ilk öneriyi ENTER tuşu ile onayla
+if (1 < 2)
+    Console.WriteLine("küçüktür");
+
+// CTRL K, S ile SNIPPET menüsü açılır
+// hazır kod parçacıkları
+#region Falan Filan İşlemi
+Console.WriteLine("uzunca");
+Console.WriteLine("bir");
+Console.WriteLine("kod");
+Console.WriteLine("bloğu");
+Console.WriteLine("hayal");
+Console.WriteLine("edin.");
+#endregion
+
